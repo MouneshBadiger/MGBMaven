33ba5041ad859a315bfffd05585d6fa936d700c1
mounesh <mounesh.gb@gmail.com>
2018-02-26 11:05:16 +0530

    Palindrome test

---
+++ CoreJavaTests/src/com/mgb/corejava/test/PalindromeTest.java
@@ -0,0 +1,14 @@
+package com.mgb.corejava.test;
+
+import java.util.Scanner;
+
+public class PalindromeTest {
+	public static void main(String[] args) {
+		while(true) {
+			System.out.println("enter the palindome no");
+			Scanner sc=new Scanner(System.in);
+			int no=sc.nextInt();
+			
+		}
+	}
+}
